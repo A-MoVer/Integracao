caa0464513acd21df4ae5edd44fc12f3042c2087
Diogo Resende <diogooresende@utad.pt>
2025-02-04 11:14:13 +0000

    teste

--- teste.js
+++ teste.js
@@ -1,1 +1,13 @@
-// Teste de workflkkkkkkkasdasd
+// Teste de erro para o SonarQube
+
+function exemploComErro() {
+    let a = 5;
+    let b = 0;
+
+    // Divisão por zero (erro intencional)
+    let resultado = a / b;
+
+    console.log("Resultado: " + resultado);  // Isto deve gerar um "code smell" no SonarQube
+}
+
+exemploComErro();
